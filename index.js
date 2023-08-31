5e3daff8b8237ab51063ab8534c6139700260e06
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-08-31 00:56:44 -0400

    Generate random numbers; replace on click

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+function getRandomNumber(min = 0, max = 100) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
+function genInnerHtml(val) {
+  return `
+  <h2>${val}</h2>
+  `;
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   const gridElements = document.querySelectorAll("#grid-wrapper > div");
   gridElements.forEach((gridEl, idx) => {
     gridEl.id = `grid-idx-${idx}`;
+    gridEl.innerHTML = genInnerHtml(getRandomNumber());
     gridEl.addEventListener("click", () => {
       console.info(`${gridEl.id} clicked`);
+      gridEl.innerHTML = genInnerHtml(getRandomNumber());
     });
   });
 });
